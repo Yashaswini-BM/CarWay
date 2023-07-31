eae05a85bf6279458e3b99d38b870004c82a1ced
Mayank Pathak <140578364+mayankpathak198@users.noreply.github.com>
2023-07-31 17:19:11 +0530

    json changes

--- src/app/application.model.ts
+++ src/app/application.model.ts
@@ -1,7 +1,6 @@
 export class Application {
     public id : number = 0;
     public name : string= "";
-    public income : number = 0.0;
     public dob : Date = new Date();
     public gender : string = "";
     public mobileno : number = 0;

34d8186b36d2d4a5a0641103d89d1e0a3ca179b3
Yashaswini-BM <140582650+Yashaswini-BM@users.noreply.github.com>
2023-08-01 16:20:25 +0530

    final

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Login } from '../login.model';
+import { LoginService } from '../services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  cred : Login = new Login();
+  flag : boolean = false;
+  msg : string = "";
+  
+  constructor(private service : LoginService, private router : Router){
+    localStorage.clear();
+   }
+
+  authenticate() {
+    this.flag = this.service.validate(this.cred);
+    
+    if(this.flag){
+    localStorage.setItem("Name", this.cred.username);
+    localStorage.setItem("Role", this.cred.password);
+    this.router.navigate(['/userlist']);
+  } else
+  this.msg = "Invalid Username/Password";
+}
+  
 
 }
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Login } from '../login.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  credentials :Login[]=[];
+  private static baseUrl : string="http://localhost:5000";
+
+  constructor(private http:HttpClient) {
+    this.http.get<Login[]>(LoginService.baseUrl + "/credentials/").subscribe(data => this.credentials = data)
+  }
+
+  persist(credentials : Login){
+    this.http.post(LoginService.baseUrl+"/credentials/", this.credentials).subscribe(data=>data=this.credentials)
+  }
+
+  validate(cred : Login):boolean{
+    for(let i=0 ; i<this.credentials.length; i++){
+      if(cred.username==this.credentials[i].username && cred.password ==this.credentials[i].password)
+          return true;
+        }
+      return false;
+  }
+
 }
